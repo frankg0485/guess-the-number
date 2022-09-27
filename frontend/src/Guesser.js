efbd8d0300fc08c9f9a744ed07c3d5e27cd5b7ce
Frank Gao <frankg0485@gmail.com>
2022-09-26 22:56:48 -0400

    fixed all buttons and previous answer label

--- frontend/src/Guesser.js
+++ frontend/src/Guesser.js
@@ -1,20 +1,25 @@
-import React, { useLayoutEffect, useState } from 'react';
+import React, { useState } from 'react';
 
 function Guesser() {
-    const [number, setNumber] = useState("");
+    const [prevAnswer, setPrevAnswer] = useState(0);
+    const [number, setNumber] = useState(getRandomNumber);
     const [guess, setGuess] = useState("");
     const [answerMessage, setAnswerMessage] = useState("Good luck!");
     const [guessesRemaining, setGuessesRemaining] = useState(5);
-
+    const [showAnswer, setShowAnswer] = useState(false);
+  
     function getRandomNumber() {
         fetch('http://localhost:5000/')
         .then(res => res.json())
         .then(obj => {
             setNumber(obj.num);
+            console.log("the random number is " + obj.num);
         });
     }
 
     function reset() {
+      setShowAnswer(true);
+      setPrevAnswer(number);
       setGuessesRemaining(5);
       setAnswerMessage("A new number has been generated. Good luck!")
       getRandomNumber();
@@ -36,22 +41,23 @@ function Guesser() {
 
     return (
         <div>
+          <h1>Welcome to this simple number guessing game!</h1>
           <button onClick={getRandomNumber}>
             Generate random number
           </button>
-          <h1>{"Guesses remaining: " + guessesRemaining}</h1>
+          <h3>{"Guesses remaining: " + guessesRemaining}</h3>
           <input type="text" name="guess" onChange={(e) => setGuess(e.target.value)}/>
           <button onClick={makeGuess}>
             Guess!
           </button>
-          { true 
-              ? <p>{answerMessage}</p>
-              : null
-          }
-          <button>
+          <p>{answerMessage}</p>
+          <button onClick={reset}>
             Give up :&#40;
           </button>
-          <h1>{"The number was " + number}</h1>
+          { showAnswer 
+              ? <h3>{"The number was " + prevAnswer}</h3>
+              : null
+          }
         </div>
     );
 }
