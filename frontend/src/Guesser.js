25e3d5ace6444ba96168c7867bf386f9311e7647
Frank Gao <frankg0485@gmail.com>
2022-09-28 01:23:43 -0400

    fade in/out transitions & UI fixes added

--- frontend/src/Guesser.js
+++ frontend/src/Guesser.js
@@ -7,7 +7,7 @@ function Guesser() {
     const [answerMessage, setAnswerMessage] = useState("Good luck!");
     const [guessesRemaining, setGuessesRemaining] = useState(5);
     const [showAnswer, setShowAnswer] = useState(false);
-  
+
     function getRandomNumber() {
         fetch('http://localhost:5000/')
         .then(res => res.json())
@@ -18,6 +18,7 @@ function Guesser() {
     }
 
     function reset() {
+      setGuess("");
       setShowAnswer(true);
       setPrevAnswer(number);
       setGuessesRemaining(5);
@@ -27,15 +28,22 @@ function Guesser() {
 
     function makeGuess() {
       setGuessesRemaining(guessesRemaining - 1);
+
+      let newMsg = "";
+      let shouldReset = false;
       if (parseInt(guess) == number) {
-        setAnswerMessage("Correct!!");
-        reset();
+        newMsg = "Correct!!";
+        shouldReset = true;
       } else if (guessesRemaining == 1) {
-        setAnswerMessage("Game over ;(");
-        reset();
+        newMsg = "Game over ;(";
+        shouldReset = true;
       } else {
-        setAnswerMessage("Not quite...");
+        newMsg = "Not quite...";
       }
+      
+
+      if (shouldReset) { reset(); }
+      
       return false;
     }
 
@@ -45,19 +53,20 @@ function Guesser() {
           <button onClick={getRandomNumber}>
             Generate random number
           </button>
-          <h3>{"Guesses remaining: " + guessesRemaining}</h3>
-          <input type="text" name="guess" onChange={(e) => setGuess(e.target.value)}/>
-          <button onClick={makeGuess}>
+          <h3>
+            {"Guesses remaining: " + guessesRemaining}
+          </h3>
+          <input type="text" name="guess" value={guess} onChange={(e) => setGuess(e.target.value)}/>
+          <button onClick={makeGuess} disabled={!guess || isNaN(+guess)}>
             Guess!
           </button>
           <p>{answerMessage}</p>
-          <button onClick={reset}>
+          <button onClick={reset} disabled={guessesRemaining == 5}>
             Give up :&#40;
           </button>
-          { showAnswer 
-              ? <h3>{"The number was " + prevAnswer}</h3>
-              : null
-          }
+          <h3 className={showAnswer ? 'fadeIn' : 'fadeOut'} onTransitionEnd={() => setShowAnswer(false)}>
+            {"The number was " + prevAnswer}
+          </h3>
         </div>
     );
 }
