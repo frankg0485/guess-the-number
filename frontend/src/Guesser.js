1ce4a88ef296f9ada0c8c18ed7cc28a35c8fc6a8
Frank Gao <frankg0485@gmail.com>
2022-09-28 18:09:47 -0400

    minor tweaks and added lower and upper bound input fields

--- frontend/src/Guesser.js
+++ frontend/src/Guesser.js
@@ -4,7 +4,7 @@ function Guesser() {
     const messages = {
       newNumber: "A new number has been generated. Good luck!",
       wrongGuess: "Not quite...",
-      gameOver: "Game over ;(",
+      gameOver: "Better luck next time :|",
       rightGuess: "Correct!!",
     };
     const [prevAnswer, setPrevAnswer] = useState(0);
@@ -13,6 +13,9 @@ function Guesser() {
     const [answerMessage, setAnswerMessage] = useState("Good luck!");
     const [guessesRemaining, setGuessesRemaining] = useState(5);
     const [showAnswer, setShowAnswer] = useState(false);
+    const [score, setScore] = useState(0);
+    let lowerBound = 0;
+    let upperBound = 10;
 
     function getRandomNumber() {
         fetch('http://localhost:5000/')
@@ -38,6 +41,7 @@ function Guesser() {
       let shouldReset = false;
       if (parseInt(guess) == number) {
         newMsg = messages.rightGuess;
+        setScore(score + 1);
         shouldReset = true;
       } else if (guessesRemaining == 1) {
         newMsg = messages.gameOver;
@@ -52,12 +56,28 @@ function Guesser() {
       return false;
     }
 
+
     return (
         <div>
           <h1>Welcome to this simple number guessing game!</h1>
-          <button onClick={getRandomNumber}>
-            Generate random number
-          </button>
+          <div style={{display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center"}}>
+            <span style={{flex: 2}} />
+            <p style={{flex: 1}}>
+              Lower bound:
+            </p>
+            <input style={{flex: 1}} type="text" name="guess" value={guess} onChange={(e) => setGuess(e.target.value)}/>
+            <span style={{flex: 2}} />
+          </div>
+          <div style={{display: "flex", flexDirection: "row", alignItems: "center"}}>
+            <span style={{flex: 2}} />
+            <p style={{flex: 1}}>
+              Upper bound:
+            </p>
+            <input style={{flex: 1}} type="text" name="guess" value={guess} onChange={(e) => setGuess(e.target.value)}/>
+            <span style={{flex: 2}} />          </div>
+          <h2>
+            {"Score: " + score}
+          </h2>
           <h3>
             {"Guesses remaining: " + guessesRemaining}
           </h3>
